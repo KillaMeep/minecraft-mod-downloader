bdc9744555727be5033ffc27a166c87bf9c899b8
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2022-02-18 12:35:08 -0700

    Add files via upload

---
+++ pythoninstaller.bat
@@ -0,0 +1,44 @@
+@echo off
+CLS
+
+
+:init
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+ECHO Next >> "%vbsGetPrivileges%"
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+exit /B
+exit
+:gotPrivileges
+setlocal & pushd .
+cd /d %~dp0
+if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+::start
+echo Running python installer now.
+TITLE Python Installer
+IF EXIST pyinstaller.exe DEL /F pyinstaller.exe
+curl https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe -o pyinstaller.exe
+pyinstaller.exe /quiet InstallAllUsers=1 PrependPath=1
+del pyinstaller.exe
+pip install wheel
+echo python install done.
+cd ..
+start modinstaller.bat
+exit
