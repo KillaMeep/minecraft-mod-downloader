cccbd4e8748328d515b1b99576cd11983e477412
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2022-02-28 16:43:37 -0700

    shows dl progress

--- installer.py
+++ installer.py
@@ -1,9 +1,11 @@
 import os
 import time
+global dld
 with open('urls.txt') as file:
     lines = [line.rstrip() for line in file]
 for x in range (0,len(lines)):
+    dld = x+1
     os.system(f'curl -s {lines[x]} -O')
-    print(f'Downloading {x+1}/{len(lines)}',end='\r',flush=True)
-print(f'Done! Downloaded {len(lines)} mods!')
+    print(f'Downloading {dld}/{len(lines)}',end='\r',flush=True)
+print(f'Done! Downloaded {dld}/{len(lines)} mods!')
 time.sleep(5)
