0ab91294bdefd2c50a87a5ea3128551d1f41273a
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2022-02-28 16:37:06 -0700

    looks a lil nicer

--- installer.py
+++ installer.py
@@ -1,7 +1,9 @@
 import os
+import time
 with open('urls.txt') as file:
     lines = [line.rstrip() for line in file]
 for x in range (0,len(lines)):
     os.system(f'curl -s {lines[x]} -O')
     print(f'Downloading {x}/{len(lines)}',end='',flush=True)
 print(f'Done! Downloaded {len(lines)} mods!')
+time.sleep(5)
