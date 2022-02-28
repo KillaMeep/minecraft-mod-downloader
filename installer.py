816b41faed2437469051c787d62b5822a60fc507
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2022-02-28 16:36:35 -0700

    Update installer.py

--- installer.py
+++ installer.py
@@ -3,3 +3,5 @@ with open('urls.txt') as file:
     lines = [line.rstrip() for line in file]
 for x in range (0,len(lines)):
     os.system(f'curl -s {lines[x]} -O')
+    print(f'Downloading {x}/{len(lines)}',end='',flush=True)
+print(f'Done! Downloaded {len(lines)} mods!')
