4d8f7c5de5be35b029a5e9d4fe64a3fc5fd23ac9
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2022-02-28 17:54:34 -0700

    Add files via upload

--- installer.py
+++ installer.py
@@ -3,14 +3,15 @@ import time
 global dld
 global lines
 global total
+dld=0
 with open('urls.txt') as file:
     lines = [line.rstrip() for line in file]
 of = input('Install Optifine? (Y/N): ').lower()
 total = len(lines)
 if of == 'y':
     total+=1
+    dld+=1
     os.system('curl -s https://optifine.net/download?f=OptiFine_1.18.1_HD_U_H4.jar --output optifine.jar')
-    dld = 1
 for x in range (0,len(lines)):
     dld+=1
     os.system(f'curl -s {lines[x]} -O')
