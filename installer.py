22c562f832a441f0766adc4d81300484a1a0623b
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2022-04-10 07:03:35 -0600

    Add files via upload

--- installer.py
+++ installer.py
@@ -1,52 +1,12 @@
 import os
 import time
-import hashlib
-import requests
-
-
+global lines
 #check system#
 if os.name == 'nt':
     pass
 else:
     print('Cannot run on linux!')
     quit()
-
-def md5Checksum(filePath,url):
-    m = hashlib.md5()
-    if url==None:
-        with open(filePath, 'rb') as fh:
-            m = hashlib.md5()
-            while True:
-                data = fh.read(8192)
-                if not data:
-                    break
-                m.update(data)
-            return m.hexdigest()
-    else:
-        r = requests.get(url)
-        for data in r.iter_content(8192):
-             m.update(data)
-        return m.hexdigest()
-
-local = md5Checksum("modinstaller.bat",None)
-remote = md5Checksum(None,"https://raw.githubusercontent.com/KillaMeep/minecraft-mod-downloader/main/modinstaller.bat")
-def update():
-    os.system('del modinstaller.bat')
-    os.system('curl -s https://raw.githubusercontent.com/KillaMeep/minecraft-mod-downloader/main/modinstaller.bat -O')
-    os.system('start modinstaller.bat')
-    os.system('exit')
-
-
-#update modinstaller.bat
-if local != remote:
-    update()
-    quit()
-else:
-    pass
-global lines
-
-
-
 dld=0
 with open('urls.txt') as file:
     lines = [line.rstrip() for line in file]
