a8f88da57ada3609728cf8ca7cabd21b3980b716
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2022-04-10 17:56:56 -0600

    Update installer.py

--- installer.py
+++ installer.py
@@ -29,7 +29,7 @@ def of_check():
     if of == 'y':
         total+=1
         dld+=1
-        os.system('curl -s https://optifine.net/downloadx?f=OptiFine_1.18.1_HD_U_H6.jar --output optifine.jar')
+        os.system('curl -s https://raw.githubusercontent.com/KillaMeep/minecraft-mod-downloader/main/mods/OptiFine_1.18.1_HD_U_H6.jar --output optifine.jar')
     elif of == 'n':
         pass
     else:
