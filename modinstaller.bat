bb6d39f592604d72a9a6fdbc9f4ad1ef1c27decc
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2022-04-23 15:38:40 -0600

    Update modinstaller.bat

--- modinstaller.bat
+++ modinstaller.bat
@@ -46,15 +46,6 @@ exit
 :gotpy
 pip install requests --quiet
 set scriptpath=%~dp0
-set CUR_YYYY=%date:~10,4%
-set CUR_MM=%date:~4,2%
-set CUR_DD=%date:~7,2%
-set CUR_HH=%time:~0,2%
-if %CUR_HH% lss 10 (set CUR_HH=0%time:~1,1%)
-set CUR_NN=%time:~3,2%
-set CUR_SS=%time:~6,2%
-set CUR_MS=%time:~9,2%
-set SUBFILENAME=%CUR_YYYY%%CUR_MM%%CUR_DD%-%CUR_HH%%CUR_NN%%CUR_SS%
 c:
 cd %appdata%
 cd .minecraft
@@ -62,7 +53,6 @@ if not exist mods mkdir mods
 cd mods
 if exist modinstaller-files rmdir /Q /S modinstaller-files 
 mkdir modinstaller-files
-if exist *.jar mkdir mods-old-%SUBFILENAME% && copy *.jar mods-old-%SUBFILENAME% && del *.jar && cls
 curl -s https://raw.githubusercontent.com/KillaMeep/minecraft-mod-downloader/main/urls.txt -O
 curl -s https://raw.githubusercontent.com/KillaMeep/minecraft-mod-downloader/main/installer.py -O
 python installer.py %scriptpath%
