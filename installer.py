1ad7da4c60d7e7c95a749a81eea817be589db756
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2022-04-23 15:41:37 -0600

    Update installer.py

--- installer.py
+++ installer.py
@@ -30,6 +30,7 @@ def of_check():
         total+=1
         dld+=1
         os.system('curl -s https://raw.githubusercontent.com/KillaMeep/minecraft-mod-downloader/main/mods/OptiFine_1.18.1_HD_U_H6.jar --output optifine.jar')
+        print('Backup successful. Saved mods to "mods-old-{filename}"')
     elif of == 'n':
         pass
     else:
@@ -39,8 +40,7 @@ def backup_old():
     backup = input('Backup old mods? (Y/N): ').lower()
     if backup == 'y':
         filename = time.strftime("%Y%m%d-%H%M%S")
-        os.system(f'mkdir mods-old-{filename}')
-        os.system(f'if exist *.jar mkdir mods-old-%SUBFILENAME% && copy *.jar mods-old-{filename} && del *.jar && cls')
+        os.system(f'if exist *.jar mkdir mods-old-{filename} && copy *.jar mods-old-{filename} && del *.jar && cls')
     elif backup == 'n':
         os.system('if exist *.jar del *.jar && cls')
     else:
