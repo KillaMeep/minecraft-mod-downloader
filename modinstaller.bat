664e41e3d198d3a55daecb44660ff79a95e2a3ef
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2022-04-28 18:38:38 -0600

    Update modinstaller.bat

--- modinstaller.bat
+++ modinstaller.bat
@@ -54,10 +54,8 @@ if not exist mods mkdir mods
 cd mods
 if exist modinstaller-files rmdir /Q /S modinstaller-files 
 mkdir modinstaller-files
-cd modinstaller-files && if not exist updater.py curl -s https://raw.githubusercontent.com/KillaMeep/minecraft-mod-downloader/main/updater.py -O
 curl -s https://raw.githubusercontent.com/KillaMeep/minecraft-mod-downloader/main/urls.txt -O
 curl -s https://raw.githubusercontent.com/KillaMeep/minecraft-mod-downloader/main/installer.py -O
-python modinstaller-files/updater.py %scriptpath%
-python installer.py
+python installer.py %scriptpath%
 del urls.txt
 del installer.py
